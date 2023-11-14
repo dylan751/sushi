140ff51c53142a7a50cbfd6edc5c86fceb106c75
Nguyen Duong <muoi07052001@gmail.com>
2023-11-14 17:56:35 +0700

    feat: add account settings page

---
+++ src/pages/[orgUniqueName]/account-settings/[tab].tsx
@@ -0,0 +1,16 @@
+// ** Demo Components Imports
+import Error404 from 'src/pages/404'
+import AccountSettings from 'src/views/pages/account-settings/AccountSettings'
+
+const tab = ['account', 'security']
+
+const AccountSettingsTab = () => {
+  const currentTab = window.location.pathname.split('/')[3]
+  if (!tab.includes(currentTab)) {
+    return <Error404 />
+  }
+
+  return <AccountSettings tab={currentTab} />
+}
+
+export default AccountSettingsTab
