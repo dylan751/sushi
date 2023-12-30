b959d754807f009029ae0094f7df0dd83f889c60
Nguyen Duong <muoi07052001@gmail.com>
2023-12-30 21:16:52 +0700

    fix: bug not able to redirect to /new

--- src/layouts/components/acl/getUserHomeRoute.tsx
+++ src/layouts/components/acl/getUserHomeRoute.tsx
@@ -6,14 +6,29 @@ export const defaultHomeRoute = 'dashboards/analytics'
 export const selectOrganizationRoute = 'organization'
 export const loginRoute = 'login'
 
+export const createOrganizationRoute = '/organization/new/'
+
 /**
  *  Set Home URL based on User Roles
  */
 const getUserHomeRoute = (organization: OrganizationProfileResponseDto | undefined, currentPath: string) => {
+  /**
+   * If user has permission for that organization, log them in
+   */
   if (organization) {
     return currentPath
   }
 
+  /**
+   * Acceptable paths
+   */
+  if (currentPath === createOrganizationRoute) {
+    return currentPath
+  }
+
+  /**
+   * Default redirect path
+   */
   return `/${selectOrganizationRoute}`
 }
 
