856b7180c2e0d7024fe6b9fa91e63498641a562d
Nguyen Duong <muoi07052001@gmail.com>
2024-04-24 16:22:10 +0700

    chore: list table

--- src/views/apps/project/budget/list/BudgetListTable.tsx
+++ src/views/apps/project/budget/list/BudgetListTable.tsx
@@ -4,21 +4,27 @@ import { useState, useEffect } from 'react'
 // ** MUI Imports
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
-import TextField from '@mui/material/TextField'
 import CardHeader from '@mui/material/CardHeader'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 import IconButton from '@mui/material/IconButton'
 import { DataGrid, GridColDef } from '@mui/x-data-grid'
 import LinearProgress from '@mui/material/LinearProgress'
+import Grid from '@mui/material/Grid'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
 
 // ** Hook Imports
 import { useCurrentOrganization } from 'src/hooks'
+import { useTranslation } from 'react-i18next'
 
 // ** Store Imports
 import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch, RootState } from 'src/store'
 import { fetchBudget } from 'src/store/apps/budget'
+import { fetchCategory } from 'src/store/apps/category'
 
 // ** Type Imports
 import { BudgetResponseDto, CurrencyType, InvoiceType } from 'src/__generated__/AccountifyAPI'
@@ -37,66 +43,6 @@ import CustomChip from 'src/@core/components/mui/chip'
 interface CellType {
   row: BudgetResponseDto
 }
-const columns: GridColDef[] = [
-  {
-    flex: 0.25,
-    minWidth: 150,
-    field: 'category',
-    headerName: 'Category',
-    renderCell: ({ row }: CellType) => (
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <IconButton color={row.category.color} sx={{ marginRight: '4px' }}>
-          <Icon icon={row.category.icon} fontSize={20} color={row.category.color} />
-        </IconButton>
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Typography variant='subtitle2' sx={{ color: `${row.category.color}.main` }}>
-            {row.category.name}
-          </Typography>
-          <Typography variant='caption'>
-            {
-              <CustomChip
-                size='small'
-                skin='light'
-                color={row.category.type === InvoiceType.EXPENSE ? 'error' : 'success'}
-                label={row.category.type}
-              />
-            }
-          </Typography>
-        </Box>
-      </Box>
-    )
-  },
-  {
-    flex: 0.4,
-    minWidth: 240,
-    field: 'progressValue',
-    headerName: 'Progress',
-    renderCell: ({ row }: CellType) => (
-      <Box sx={{ width: '100%' }}>
-        <Typography variant='body2' sx={{ color: `text.${row.category.color}` }}>
-          {calculateBudgetProcess(row.category.spentAmount, row.amount)}%
-        </Typography>
-        <LinearProgress
-          variant='determinate'
-          value={calculateBudgetProcess(row.category.spentAmount, row.amount)}
-          color={row.category.color} // TODO: Use `success`, `warning`, `error` color based on budget state
-          sx={{ height: 6, borderRadius: '5px' }}
-        />
-      </Box>
-    )
-  },
-  {
-    flex: 0.25,
-    minWidth: 150,
-    field: 'amount',
-    headerName: 'amount',
-    renderCell: ({ row }: CellType) => (
-      <Typography variant='body2' sx={{ color: 'text.primary' }}>
-        {formatCurrencyAsCompact(row.amount, Locale.EN, CurrencyType.USD)}
-      </Typography>
-    )
-  }
-]
 
 export interface BudgetListTableProps {
   projectId: string
@@ -104,39 +50,143 @@ export interface BudgetListTableProps {
 
 const BudgetListTable = ({ projectId }: BudgetListTableProps) => {
   // ** State
-  const [value, setValue] = useState<string>('')
+  const [categoryId, setCategoryId] = useState<string | ''>('')
   const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 7 })
 
   // ** Hooks
   const { organizationId } = useCurrentOrganization()
+  const { t } = useTranslation()
   const dispatch = useDispatch<AppDispatch>()
   const budgetStore = useSelector((state: RootState) => state.budget)
+  const categoryStore = useSelector((state: RootState) => state.category)
+
+  const handleOnChangeCategory = (value: string | '') => {
+    setCategoryId(value)
+  }
 
   useEffect(() => {
-    dispatch(fetchBudget({ organizationId, projectId: parseInt(projectId) }))
-  }, [dispatch, organizationId, projectId, value])
+    const fetchBudgetParams: any = {
+      organizationId,
+      projectId: parseInt(projectId)
+    }
+    if (categoryId) {
+      fetchBudgetParams.categoryId = parseInt(categoryId)
+    }
+    dispatch(fetchBudget(fetchBudgetParams))
+    dispatch(fetchCategory({ organizationId, projectId: parseInt(projectId) }))
+  }, [dispatch, organizationId, projectId, categoryId])
+
+  useEffect(() => {
+    dispatch(fetchCategory({ organizationId, projectId: parseInt(projectId) }))
+  }, [dispatch, organizationId, projectId])
+
+  const columns: GridColDef[] = [
+    {
+      flex: 0.25,
+      minWidth: 150,
+      field: 'category',
+      headerName: t('project_page.budget.category') as string,
+      renderCell: ({ row }: CellType) => (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton color={row.category.color} sx={{ marginRight: '4px' }}>
+            <Icon icon={row.category.icon} fontSize={20} color={row.category.color} />
+          </IconButton>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography variant='subtitle2' sx={{ color: `${row.category.color}.main` }}>
+              {row.category.name}
+            </Typography>
+            <Typography variant='caption'>
+              {
+                <CustomChip
+                  size='small'
+                  skin='light'
+                  color={row.category.type === InvoiceType.EXPENSE ? 'error' : 'success'}
+                  label={row.category.type}
+                />
+              }
+            </Typography>
+          </Box>
+        </Box>
+      )
+    },
+    {
+      flex: 0.4,
+      minWidth: 240,
+      field: 'progressValue',
+      headerName: t('project_page.budget.progress') as string,
+      renderCell: ({ row }: CellType) => (
+        <Box sx={{ width: '100%' }}>
+          <Typography variant='body2' sx={{ color: `text.${row.category.color}` }}>
+            {calculateBudgetProcess(row.category.spentAmount, row.amount)}%
+          </Typography>
+          <LinearProgress
+            variant='determinate'
+            value={calculateBudgetProcess(row.category.spentAmount, row.amount)}
+            color={row.category.color} // TODO: Use `success`, `warning`, `error` color based on budget state
+            sx={{ height: 6, borderRadius: '5px' }}
+          />
+        </Box>
+      )
+    },
+    {
+      flex: 0.25,
+      minWidth: 150,
+      field: 'amount',
+      headerName: t('project_page.budget.amount') as string,
+      renderCell: ({ row }: CellType) => (
+        <Typography variant='body2' sx={{ color: 'text.primary' }}>
+          {formatCurrencyAsCompact(row.amount, Locale.EN, CurrencyType.USD)}
+        </Typography>
+      )
+    }
+  ]
 
   return (
-    <Card>
-      <CardHeader title='Budget List' />
-      <CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-          <Typography variant='body2' sx={{ mr: 2 }}>
-            Search:
-          </Typography>
-          <TextField size='small' placeholder='Search Project' value={value} onChange={e => setValue(e.target.value)} />
-        </Box>
-      </CardContent>
-      <DataGrid
-        autoHeight
-        rows={budgetStore.budgets}
-        columns={columns}
-        disableRowSelectionOnClick
-        pageSizeOptions={[7, 10, 25, 50]}
-        paginationModel={paginationModel}
-        onPaginationModelChange={setPaginationModel}
-      />
-    </Card>
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title={t('project_page.budget.filters')} />
+          <CardContent>
+            <Grid container spacing={6}>
+              <Grid item xs={12} sm={6}>
+                <FormControl fullWidth>
+                  <InputLabel id='invoice-status-select'>{t('project_page.budget.category_type')}</InputLabel>
+
+                  <Select
+                    fullWidth
+                    value={categoryId}
+                    sx={{ mr: 4, mb: 2 }}
+                    label='Category Type'
+                    onChange={e => handleOnChangeCategory(e.target.value)}
+                    labelId='category-type-select'
+                  >
+                    <MenuItem value=''>All Categories</MenuItem>
+                    {categoryStore.categories.map(category => (
+                      <MenuItem value={category.id} key={category.id}>
+                        {category.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <DataGrid
+            autoHeight
+            rows={budgetStore.budgets}
+            columns={columns}
+            disableRowSelectionOnClick
+            pageSizeOptions={[7, 10, 25, 50]}
+            paginationModel={paginationModel}
+            onPaginationModelChange={setPaginationModel}
+          />
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
 
