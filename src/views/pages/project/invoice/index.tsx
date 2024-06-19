f3707ae7f69d355d683da64445e045c1774387de
Duong (Dylan) <muoi07052001@gmail.com>
2024-06-19 11:00:31 +0700

    fix: align right invoice total (#247)

--- src/views/pages/project/invoice/index.tsx
+++ src/views/pages/project/invoice/index.tsx
@@ -261,7 +261,9 @@ const InvoiceTab = ({ projectId, name }: InvoiceTabProps) => {
           {row.type === InvoiceType.EXPENSE ? '-' : '+'}
           {formatCurrencyAsStandard(row.total, Locale.EN, row.currency)}
         </Typography>
-      )
+      ),
+      headerAlign: 'right',
+      align: 'right'
     },
     {
       flex: 0.2,
